25e821008db913ed77885cd29756a9c4d6227a7b
Sierra Wallace <sskwallace@gmail.com>
2020-10-04 00:56:39 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,8 +1,8 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 class Kid 
+  include Dance
   extend MetaDancing
-  include Dance 
   attr_accessor :name 
   def initialize(name)
     @name = name
